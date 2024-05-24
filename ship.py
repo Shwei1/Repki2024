df7ea1bcc79d8b35da4d69e9b9b3bbe2957ce1e9
daria zashkolna <inkognitodz28@gmail.com>
2024-05-24 16:46:13 +0300

    added ship

--- ship.py
+++ ship.py
@@ -8,26 +8,47 @@ class Ship:
         '''
         Корабель має розмір та список позицій на полі.
 
-        :param start_pos: початкова позиція
-        :param orient: напрямок корабля, HORIZONT = 0, VERTICAL = 1
+        :param start_pos: початкова позиція, де (x, y) - координати матриці
+        :param orient: напрямок корабля, HORIZONTAL = 0, VERTICAL = 1
         :param size: розмір (к-сть клітинок)
         '''
 
         self.start_pos = start_pos
-        self.orien = orient
+        self.orient = orient
         self.size = size
 
     def get_body(self):
 
         '''
-        розміщує корабель, маючи стартову позицію i розмір
+        визначає координати, які займає один корабель
         '''
 
-        pass
+        if self.orient == HORIZONTAL:
+            for i in range(self.size):
+                yield (self.start_pos[0], self.start_pos[1] + i)
+        else:
+            for i in range(self.size):
+                yield (self.start_pos[0] + i, self.start_pos[1])
 
     def get_area(self):
         '''
         визначає заборонені клітинки по периметру корабля
         '''
 
-        pass
+        if self.orient == HORIZONTAL:
+            x1, x2 = self.start_pos[0] - 1, self.start_pos[0] + 1
+            y1, y2 = self.start_pos[1] - 1, self.start_pos[1] + self.size
+        else:
+            x1, x2 = self.start_pos[0] - 1, self.start_pos[0] + self.size
+            y1, y2 = self.start_pos[1] - 1, self.start_pos[1] + 1
+
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                    yield (i, j)
+
+
+if __name__ == '__main__':
+    for x in Ship((1,1), 1, 4).get_body():
+        print(x)
+    for x in Ship((1,1), 1, 4).get_area():
+        print(x)
