52202d228f972a36f5179861d76f7a92be1abd0f
xshwei <portal.gamy@gmail.com>
2024-05-27 00:00:28 +0300

    Added inspector method for ship placement. Minor changes for constants. Another file for managing fields to avoid circular imports.

---
+++ manager.py
@@ -0,0 +1,44 @@
+from window import window
+from constants import *
+from ship import Ship
+from field import field
+from board import Board
+
+def check_area(ship: Ship):
+    """
+    Перевірка наявності кораблів в околі заданого корабля
+    """
+    fahrwasser = ship.get_area()   # змінна з координатами в околі корабля
+    for coordinate_set in fahrwasser:
+        x, y = coordinate_set[0], coordinate_set[1]
+        if field[x][y] > 0:
+            return False
+    else:
+        return True
+
+
+def fleet_dictionary_manager(dictionary):
+    """
+    Сюди, можливо, перенесеться блок коду, що оновлює словник з кораблями на заданому полі
+    """
+    ...
+    pass
+
+
+def check_fleet(board: Board):
+    for i in range(1, 5):
+        if board.ships[i] >= FLEET[i]:
+            return False
+        else:
+            return True
+    pass
+
+
+
+
+if __name__ == "__main__":
+    for k in Ship((0, 0), 1, 1).get_area():
+        print(k)
+
+
+
